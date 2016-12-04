60d652e0a6bbcf790696d9e200d0358ecf1c423c
Ibro <ibro@ibro.me>
2016-12-04 13:50:50 +0100

    track circle movement

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs';
 
+let circle = document.getElementById('circle');
 let source = Observable
     .fromEvent(document, 'mousemove')
     .map((e: MouseEvent) => {
@@ -10,8 +11,14 @@ let source = Observable
     })
     .filter(value => value.x < 500);
 
+
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
+
 source.subscribe(
-    value => console.log(value),
+    onNext,
     e => console.log(`error: ${e}`),
     () => console.log('complete')
 );
