652f8e575a93cf707273064dffa4b7c76cb4eaf6
Ibro <ibro@ibro.me>
2016-12-04 12:58:04 +0100

    Simple observer with Observable.create

--- main.ts
+++ main.ts
@@ -6,23 +6,13 @@ import { Observable } from 'rxjs';
 
 let numbers = [1, 5, 10];
 let source = Observable.create(observer => {
+    for(let n of numbers) {
+        observer.next(n);
+    }
 
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
+    observer.complete();
 
-        if (index < numbers.length) {
-            setTimeout(produceValue, 250);
-        }
-        else {
-            observer.complete();
-        }
-    };
-
-    produceValue();
-
-}).map(n => n * 2)
-    .filter(n => n > 4);
+});
 
 
 source.subscribe(
